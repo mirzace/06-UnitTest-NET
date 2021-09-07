598a1783b9c5af064a50f1068c3def2e3627aadd
mirzace <mirzace@maestralsolutions.com>
2021-09-07 20:08:24 +0200

    Initial Commit

---
+++ Sparky/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class Customer
+    {
+        public string GreetMessage { get; set; }
+        public int Discount { get; set; } = 15;
+        public int OrderTotal { get; set; }
+
+        public string GreetAndCombineNames(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException("Empty First Name");
+            }
+
+            GreetMessage = $"Hello, {firstName} {lastName}";
+            Discount = 20;
+            return GreetMessage;
+        }
+
+        public CustomerType GetCustomerDetails()
+        {
+            if (OrderTotal < 100)
+            {
+                return new BasicCustomer();
+            }
+            return new PlatinumCustomer();
+        }
+    }
+    public class CustomerType { }
+    public class BasicCustomer : CustomerType { }
+    public class PlatinumCustomer : CustomerType { }
+}
